e138a6d5d6ff7c2e7193830868e3d2dad5b92294
Glitch (image-abstractor) <none>
2018-05-09 18:48:33 +0000

    🏨👗 Checkpoint ./src/routes/imgSearchRoute.js:852327/904

--- src/routes/imgSearchRoute.js
+++ src/routes/imgSearchRoute.js
@@ -7,7 +7,9 @@ imgSearchRouter.get(/\w/, function(req, res) {
   
   const query = req.query;
   const searchTerm = req.params.search;
-  console.log(req.keys);
+  // console.log(Object.keys(req));
+  console.log(req._parsedUrl.pathname);
+  console.log(req.query.id);
   var cache = [];
   res.end(JSON.stringify(req, function(key, value) {
     if (typeof value === 'object' && value !== null) {
@@ -22,27 +24,5 @@ imgSearchRouter.get(/\w/, function(req, res) {
     }, 4));
   cache = null;
   })
-
-function syntaxHighlight(json) {
-    if (typeof json != 'string') {
-         json = JSON.stringify(json, undefined, 2);
-    }
-    json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
-    return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
-        var cls = 'number';
-        if (/^"/.test(match)) {
-            if (/:$/.test(match)) {
-                cls = 'key';
-            } else {
-                cls = 'string';
-            }
-        } else if (/true|false/.test(match)) {
-            cls = 'boolean';
-        } else if (/null/.test(match)) {
-            cls = 'null';
-        }
-        return '<span class="' + cls + '">' + match + '</span>';
-    });
-}
 
 module.exports = imgSearchRouter;
