acdb9690df9e1064d0b43f17beca52d129e31220
Glitch (image-abstractor) <none>
2018-05-09 16:25:40 +0000

    📼👗 Checkpoint ./src/routes/imgSearchRoute.js:852327/173

--- src/routes/imgSearchRoute.js
+++ src/routes/imgSearchRoute.js
@@ -2,8 +2,11 @@ const express = require('express');
 const imgSearchRouter = express.Router();
 
 
-imgSearchRouter.get(function(req, res) {
-      res.send('Here'); 
+imgSearchRouter.get(/\w/, function(req, res) {
+  const query = req.query;
+  const searchTerm = req.params.search;
+  
+  res.json(JSON.stringify(req)); 
   })
 
 
