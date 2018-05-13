b5f8fe8c873701bf011629272760e48c9be9841e
Glitch (image-abstractor) <none>
2018-05-13 18:19:03 +0000

    👯🙋 Checkpoint ./src/routes/latestSearchRoute.js:852327/1420

--- src/routes/latestSearchRoute.js
+++ src/routes/latestSearchRoute.js
@@ -8,7 +8,25 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
 latestSearchRouter.get('/', function (req, res) {
-  
+  // Connection URL. This is where your mongodb server is running.
+    const search_db = `mongodb://${process.env.USER}:${process.env.PASSWORD}@ds119640.mlab.com:19640/image_abs_search_db`;
+    const dbName = 'image_abs_search_db';
+    // Use connect method to connect to the Server
+    (async function mongo() {
+        let client;
+        try {
+          client = await MongoClient.connect(search_db);
+          console.log('Connected correctly to server again');
+          const db = client.db(dbName);
+          var dbCount = await db.collection('searches').count();
+          const response = await db.collection('searches').find().skip(db.collection('searches').count() - 1);
+          res.send(response);
+          db.close();
+        } catch (err) {
+          debug(err.stack)
+        }
+        client.close();
+      }());
 })
 
 module.exports = latestSearchRouter;
